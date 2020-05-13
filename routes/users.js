7aedf9ea17608270a96f1e634c251afaee76449e
unknown <vincentlam30111998@gmail.com>
2020-05-13 14:38:19 +0700

    Change password API

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
 var redis = require('../utils/redis')
+var bcrypt = require('bcrypt');
+
+var { response, DEFINED_CODE } = require('../config/response');
 
 var userModel = require('../models/userModel');
+
+const saltRounds = 15;
 
 /* GET users listing. */
 
@@ -28,4 +33,37 @@ router.get('/getUser', (req, res, next) => {
     })
   })
 })
+
+/* Change password */
+router.put('/changePassword', (req, res, next) => {
+  var token = req.headers.authorization.slice(7);
+  var decodedPayload = jwt.decode(token, {
+    secret: 'S_Team',
+  });
+  var id_user = decodedPayload.id;
+  var newPassword = req.body.new_password;
+  var oldPassword = req.body.old_password;
+  console.log(req.user.password);
+  bcrypt.compare(oldPassword, req.user.password, (err, result) => {
+    if (result) {
+      bcrypt.hash(newPassword, saltRounds, (err, hash) => {
+        if (err) {
+          res.json(err);
+        } else {
+          var updates = [{field: 'password', value: `'${hash}'`}];
+          userModel.updateUserInfo(id_user, updates)
+          .then(data => {
+            redis.setKey(token);
+            response(res, DEFINED_CODE.CHANGE_PASSWORD_SUCCESS);
+          }).catch(err => {
+            response(res, DEFINED_CODE.ACCESS_DB_FAIL, err);
+          })
+        }
+      })
+    } else {
+      response(res, DEFINED_CODE.CHANGE_PASSWORD_FAIL, `Change password failed!`);
+    }
+  })
+})
+
 module.exports = router;
