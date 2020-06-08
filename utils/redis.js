96134a5b1f924de25a71e9511a90b64438daee8f
Bao <baoit128@gmail.com>
2020-06-08 11:13:53 +0700

    fix redis

--- utils/redis.js
+++ utils/redis.js
@@ -40,8 +40,14 @@ module.exports = {
     },
     getKey: (value) => {
         return new Promise((resolve, reject) => {
-            var client = redis.createClient();
-            client.on('connect', (err) => {
+            var client;
+            if (process.env.REDISCLOUD_URL) {
+                client = redis.createClient(process.env.REDISCLOUD_URL, { no_ready_check: true });
+
+            }
+            else {
+                client = redis.createClient();
+            } client.on('connect', (err) => {
                 if (err) {
                     console.log('Something went wrong ' + err);
                     throw err;
