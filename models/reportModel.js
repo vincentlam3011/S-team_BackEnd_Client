bb0e3f87db3a6f935d642f7a29fbf619df963b70
1612041_phbinh <1612041_phbinh>
2020-07-07 06:17:19 +0700

    fix bug: add report

--- models/reportModel.js
+++ models/reportModel.js
@@ -2,7 +2,7 @@ var db = require('../utils/db');
 
 module.exports = {
     addReport: (id_user1, role1, id_user2, role2, content, type, applicantId) => {
-        let addReportQuery = `insert into reports (id_user1, role1, id_user2, role2, content, type) values (${id_user1},${role1}, ${id_user2}, ${role2}, N'${content}', ${type}, ${applicantId})`;
+        let addReportQuery = `insert into reports (id_user1, role1, id_user2, role2, content, type, id_applicant) values (${id_user1},${role1}, ${id_user2}, ${role2}, N'${content}', ${type}, ${applicantId})`;
 
         return db.query(addReportQuery);
     },
